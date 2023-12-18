f2338c007ad540e89ac41f62cc211e26ec0cdbcf
PopeAfSweden <kristoffer@artmark.nu>
2023-12-18 11:48:15 +0100

    using Demo MG

--- main.tf
+++ main.tf
@@ -23,35 +23,6 @@ module "enterprise_scale" {
   root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = var.root_id
   root_name      = var.root_name
-  library_path   = "${path.root}/lib"
 
-  custom_landing_zones = {
-    "${var.root_id}-online" = {
-      display_name               = "${upper(var.root_id)} Online Example 1"
-      parent_management_group_id = "${var.root_id}-landing-zones"
-      subscription_ids           = []
-      archetype_config = {
-        archetype_id   = "customer_online"
-        parameters     = {}
-        access_control = {}
-      }
-    }
-    "${var.root_id}-online-example-2" = {
-      display_name               = "${upper(var.root_id)} Online Example 2"
-      parent_management_group_id = "${var.root_id}-landing-zones"
-      subscription_ids           = []
-      archetype_config = {
-        archetype_id = "customer_online"
-        parameters = {
-          Deny-Resource-Locations = {
-            listOfAllowedLocations = ["eastus", ]
-          }
-          Deny-RSG-Locations = {
-            listOfAllowedLocations = ["eastus", ]
-          }
-        }
-        access_control = {}
-      }
-    }
-  }
+  deploy_demo_landing_zones = true
 }
